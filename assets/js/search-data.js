6706e617f178050916fd3ba21c04bece10a28f76
selmatekir <61739726+selmatekir@users.noreply.github.com>
2025-03-06 18:56:00 +0000

    Deploying to gh-pages from @ selmatekir/selmatekir.github.io@8ef563ffef5363ef0197d790c992cefbef676bf9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,14 +490,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%65%6C%6D%61%74%65%6B%69%72@%69%79%74%65.%65%64%75.%74%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/https://www.linkedin.com/in/selma-tekir-b4626015/", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/https://orcid.org/0000-0002-0488-9682", "_blank");
         },
       },{
         id: 'social-rss',
@@ -511,14 +518,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=iRawdD8AAAAJ&hl=tr&oi=ao your Google Scholar ID", "_blank");
         },
       },{
       id: 'light-theme',
